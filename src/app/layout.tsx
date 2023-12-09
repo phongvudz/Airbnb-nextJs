2e358e254e48c465d8c53c821a2ec2b9324225df
phongvudz <nguyennguyen969873@gmail.com>
2023-12-09 16:05:57 +0700

    nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ClientOnly from "@/components/ClientOnly";
+import Navbar from "@/components/navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
+        <div className="pb-20 pt-28">{children}</div>
+      </body>
     </html>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 type NavbarProps = {};
 
@@ -8,7 +11,11 @@ const Navbar = ({}: NavbarProps) => {
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b">
         <Container>
-          <div className="flex flex-row items-center gap-3 justify-between"></div>
+          <div className="flex flex-row items-center gap-3 justify-between">
+            <Logo />
+            <Search />
+            <UserMenu />
+          </div>
         </Container>
       </div>
     </div>
